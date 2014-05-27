317cfe3e36b0bd9c3ade5c83c5156f06fc6e3df3
Andrew Raynal <andrewraynal@Andrews-MacBook-Air.local>
2014-05-27 09:42:43 -0400

    update

--- public/js/services/itemssvc.js
+++ public/js/services/itemssvc.js
@@ -1,6 +1,6 @@
 angular.module('angularstoreApp')
 	.factory('ItemsSvc', function($resource) {
-		return $resource('mongodb://FrankZappa:andrewandrew44@ds053218.mongolab.com:53218/andrewr', {},
+		return $resource('api/collections/BoardStore', {},
 			{
 				query: {
 						method: 'GET',
--- public/js/services/itemsvc.js
+++ public/js/services/itemsvc.js
@@ -1,6 +1,6 @@
 angular.module('angularstoreApp')
 	.factory('ItemSvc', function($resource) {
-		return $resource('mongodb://FrankZappa:andrewandrew44@ds053218.mongolab.com:53218/andrewr/:id',
+		return $resource('api/collections/BoardStore:id',
 			{
 				id: '@_id'
 			},
