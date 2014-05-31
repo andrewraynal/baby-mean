96403ca6076e13604649c40e293ae65740717fb8
Andrew Raynal <andrewraynal@Andrews-MacBook-Air.local>
2014-05-31 17:21:59 -0400

    control panel

--- public/js/controllers/allctrl.js
+++ public/js/controllers/allctrl.js
@@ -66,6 +66,30 @@ angular.module('angularstoreApp')
 //and one that follows is not visible by setting the "isVisible" to false
   })
 
+.controller('DropdownCtrl', function ($scope, $location){
+  
+  $scope.dropper = function()[{
+    $location.path('/newproductitem');
+    // create new product
+  },
+    $location.path('/shop');
+  ];
+
+  $scope.status = {
+    isopen: false
+  };
+
+  $scope.toggled = function(open) {
+    console.log('Dropdown is now: ', open);
+  };
+
+  $scope.toggleDropdown = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    $scope.status.isopen = !$scope.status.isopen;
+  };
+}    
+  })
 
 
 
