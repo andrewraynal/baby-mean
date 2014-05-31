eb144f7621448ab8b21402a1c4a33fe2c23f8f05
Andrew Raynal <andrewraynal@Andrews-MacBook-Air.local>
2014-05-31 18:22:08 -0400

    dropdowns

--- public/js/controllers/allctrl.js
+++ public/js/controllers/allctrl.js
@@ -66,29 +66,6 @@ angular.module('angularstoreApp')
 //and one that follows is not visible by setting the "isVisible" to false
   })
 
-.controller('DropdownCtrl', function ($scope){
-  
-  $scope.dropper = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
-
-  $scope.status = {
-    isopen: false
-  };
-
-  $scope.toggled = function(open) {
-    console.log('Dropdown is now: ', open);
-  };
-
-  $scope.toggleDropdown = function($event) {
-    $event.preventDefault();
-    $event.stopPropagation();
-    $scope.status.isopen = !$scope.status.isopen;
-  };
-}    
-  })
 
 
 
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -7,6 +7,19 @@ angular.module('angularstoreApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.status = {
+    isopen: false
+    };
+
+    $scope.toggled = function(open) {
+    console.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      $scope.status.isopen = !$scope.status.isopen;
+    };
   });
  
 
